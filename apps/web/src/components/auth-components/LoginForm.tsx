ea125e7b9e84b5b96992440d3152ec3d1409c7f0
Izam Ghali <izamghali18@gmail.com>
2024-05-02 13:17:16 +0700

    style: added event, login, signup forms

---
+++ apps/web/src/components/auth-components/LoginForm.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import Button from "../Button"
+import { showCloseModal } from "@/helpers/modal.function"
+
+export default function LoginForm({ className }: { className: string }) {
+
+    function showSignUpModal() {
+        showCloseModal('sign-up-as-modal', 'login-modal')
+    }
+
+    return (
+        <div className={`${className}`}>
+            <label className="input input-bordered flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+                <input type="text" className="grow" placeholder="Email" />
+            </label>
+            <label className="input input-bordered flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
+                <input type="password" className="grow" placeholder="Password" />
+            </label>
+
+            <div className="flex justify-end mt-8">
+                <Button className="w-full bg-accent" text="Login" />
+            </div>
+            <p className="text-center">Don't have account?&nbsp;
+                <button onClick={showSignUpModal} className="underline cursor-pointer">Sign Up</button>
+            </p>
+        </div>
+    )
+};
+
