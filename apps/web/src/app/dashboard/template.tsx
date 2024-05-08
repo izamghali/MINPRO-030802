4e098df3abf79cc86cbda7269fa1428d0ba17333
Izam Ghali <izamghali18@gmail.com>
2024-05-08 18:14:17 +0700

    style: populated dashboard Drawer menu btn, and made it responsive

--- apps/web/src/app/dashboard/template.tsx
+++ apps/web/src/app/dashboard/template.tsx
@@ -2,19 +2,23 @@
 import React from "react"
 import Drawer from "@/components/Drawer"
 import SidebarMenuBtn from "@/components/SidebarMenuBtn"
-import { eventsTest } from "@/helpers/dummyData";
+import { isUserDrawer } from "@/helpers/dummyData";
+import { analyticsSVG, calendarSVG, percentSVG, heartBoxSVG, starSVG, billSVG, profileSVG } from "@/helpers/svg";
 
 export default function Template({ children }: { children: any }) {
 
     const testNumberEvent = 88
-    const analyticsSvg = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bar-chart-line" viewBox="0 0 16 16"><path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1zm1 12h2V2h-2zm-3 0V7H7v7zm-5 0v-3H2v3z"/></svg>
-    const eventSvg = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar-event" viewBox="0 0 16 16"><path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/><path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/></svg>;
 
     function handleIsActive(buttonID: string) {
         const sidebar = document.getElementById(buttonID)
         sidebar?.classList.add('is-active')
         console.log(sidebar)
     }
+
+    // guitar
+    const srcDrawerImgBgUser = 'https://firebasestorage.googleapis.com/v0/b/image-gallery-3010c.appspot.com/o/ismael-paramo-QIqbX8VvCMo-unsplash.jpg?alt=media&token=6887d66e-8728-4aac-a83a-443405bd5eb5'
+    // underwater
+    const srcDrawerImgBgOrganizer = 'https://images.pexels.com/photos/4666754/pexels-photo-4666754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
 
     return (
         <div>
@@ -25,28 +29,87 @@ export default function Template({ children }: { children: any }) {
                 childrenMenu={
                 <ul className={`menu p-4 w-80 min-h-full bg-base-200 text-base-content`}>
                     <h3 className="font-bold mb-10">Drawer</h3>
-                    <SidebarMenuBtn
-                        redirect="/dashboard/events"
-                        className="active:bg-transparent"
-                        func={handleIsActive}
-                        buttonID="events-btn"
-                    >
-                        {/* NOTE:  async/await fetching number of events */}
-                        <div className="flex items-center justify-between w-full group">
-                            <div className="flex items-center gap-2">
-                                { eventSvg }
-                                Events
+                    {
+                        isUserDrawer ? // if user is logged in
+                            <div className="relative">
+                                <img className="h-screen saturate-[.25] fixed top-0 left-0" src={srcDrawerImgBgUser} alt="" />
+                                <SidebarMenuBtn
+                                    text="Events" svg={calendarSVG}
+                                    redirect="/dashboard/user/events"
+                                    className="active:bg-transparent text-white"
+                                    func={handleIsActive}
+                                    buttonID="event-btn"
+                                />
+                                <SidebarMenuBtn
+                                    text="Transaction" svg={billSVG}
+                                    redirect="/dashboard/user/transaction"
+                                    className="active:bg-transparent text-white"
+                                    func={handleIsActive}
+                                    buttonID="event-btn"
+                                />
+                                <SidebarMenuBtn
+                                    text="Review" svg={starSVG}
+                                    redirect="/dashboard/user/review"
+                                    className="active:bg-transparent text-white"
+                                    func={handleIsActive}
+                                    buttonID="event-btn"
+                                />
+                                <SidebarMenuBtn
+                                    text="Wish List" svg={heartBoxSVG}
+                                    redirect="/dashboard/user/wishlist"
+                                    className="active:bg-transparent text-white"
+                                    func={handleIsActive}
+                                    buttonID="event-btn"
+                                />
+                                <SidebarMenuBtn
+                                    text="Account" svg={profileSVG}
+                                    redirect="/dashboard/user/account"
+                                    className="active:bg-transparent text-white"
+                                    func={handleIsActive}
+                                    buttonID="event-btn"
+                                />
                             </div>
-                            { testNumberEvent > 99 ? '99+' : testNumberEvent }
-                        </div>
-                    </SidebarMenuBtn>
-                    <SidebarMenuBtn
-                        text="Analytics" svg={analyticsSvg}
-                        redirect="/dashboard/analytics"
-                        className="active:bg-transparent"
-                        func={handleIsActive}
-                        buttonID="analytics-btn"
-                    />
+                        : // if organizer is logged in
+                            <div className="relative">
+                                <img className="h-screen fixed top-0 left-0 object-cover saturate-[0.30]" src={srcDrawerImgBgOrganizer} alt="" />
+                                <SidebarMenuBtn
+                                    redirect="/dashboard/organizer/events"
+                                    className="active:bg-transparent text-white"
+                                    func={handleIsActive}
+                                    buttonID="events-btn"
+                                >
+                                    {/* NOTE:  async/await fetching number of events */}
+                                    <div className="flex items-center justify-between w-full group">
+                                        <div className="flex items-center gap-2">
+                                            { calendarSVG }
+                                            Events
+                                        </div>
+                                        { testNumberEvent > 99 ? '99+' : testNumberEvent }
+                                    </div>
+                                </SidebarMenuBtn>
+                                <SidebarMenuBtn
+                                    text="Promos" svg={percentSVG}
+                                    redirect="/dashboard/organizer/promos"
+                                    className="active:bg-transparent text-white"
+                                    func={handleIsActive}
+                                    buttonID="promos-btn"
+                                />
+                                <SidebarMenuBtn
+                                    text="Analytics" svg={analyticsSVG}
+                                    redirect="/dashboard/organizer/analytics"
+                                    className="active:bg-transparent text-white"
+                                    func={handleIsActive}
+                                    buttonID="analytics-btn"
+                                />
+                                <SidebarMenuBtn
+                                    text="Account" svg={profileSVG}
+                                    redirect="/dashboard/organizer/account"
+                                    className="active:bg-transparent text-white"
+                                    func={handleIsActive}
+                                    buttonID="analytics-btn"
+                                />
+                            </div>
+                    }
                 </ul>
                 }
             >
--- apps/web/src/app/template.tsx
+++ apps/web/src/app/template.tsx
@@ -8,9 +8,11 @@ import Modal from "@/components/Modal"
 import LoginForm from "@/components/auth-components/LoginForm"
 import SignUpForm from "@/components/auth-components/SignUpForm"
 import { showCloseModal } from "@/helpers/modal.function"
-import Drawer from "@/components/Drawer"
-import SidebarMenuBtn from "@/components/SidebarMenuBtn"
 import Button from "@/components/Button"
+import EventForm from "@/components/EventForm"
+import MenuList from "@/components/MenuList"
+import { calendarSVG, billSVG, starSVG, heartBoxSVG, profileSVG, logoutSVG, searchSVG, plusSVG } from "@/helpers/svg"
+import { isUserLoggedNavbar, isUserLoggedNavbarHome } from '@/helpers/dummyData'
 
 export default function Template({ children } : Readonly<{ children : React.ReactNode}>) {
 
@@ -24,75 +26,154 @@ export default function Template({ children } : Readonly<{ children : React.Reac
         showCloseModal('login-modal')
     }
 
-    const plusSvg = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16"><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/></svg>
-    const logoutSvg = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/><path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/></svg>
-
     return (
         <div>
-            <Navbar >
+            <Navbar className="border-b-2">
                 {
-                    pathname === '/' ?
+                    pathname === '/' ? 
                         <div>
-                            <div className="gap-4 flex">
-                            
-                                {/* Sign Up Modal */}
-                                <Modal modalTitle="Sign Up As" textBtn="Sign Up"
-                                    modalID="sign-up-as-modal"
-                                    modalClassName="max-w-96 text-center"
-                                    buttonClassName="px-8 hidden md:flex"
-                                >
-                                    <div className="flex flex-col gap-4">
-
-                                        {/* Sign Up as Organizer Modal */}
-                                        <Modal modalTitle="Sign Up as Organizer" textBtn="Organizer"
-                                            modalID="sign-up-modal-organizer"
-                                            modalClassName="max-w-96"
-                                            buttonClassName="w-full"
-                                            modalIDToClose="sign-up-as-modal"
-                                            formID="sign-up-form-organizer"
-                                        >
-                                            <SignUpForm className="flex flex-col sm:gap-4 gap-2" role="organizer"  />
-                                        </Modal>
-                                        <p className="text-center">or</p>
-
-                                        {/* Sign Up as User Modal */}
-                                        <Modal modalTitle="Sign Up as Adventurer" textBtn="Adventurer"
-                                            modalID="sign-up-modal-adventurer"
-                                            modalClassName="max-w-96"
-                                            buttonClassName="w-full"
-                                            modalIDToClose="sign-up-as-modal"
-                                            formID="sign-up-form-user"
-                                        >
-                                            <SignUpForm className="flex flex-col sm:gap-4 gap-2" role="adventurer" />
-                                        </Modal>
+                            {
+                                isUserLoggedNavbarHome ? 
+                                    // if user is logged in
+                                    <div className="flex gap-4">
+                                        <Dropdown 
+                                            className="dropdown-end" 
+                                            ulClassName=" flex gap-4"
+                                            buttonSvg={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16"><path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/></svg>}>
+                                            <MenuList svg={calendarSVG}>Events</MenuList>
+                                            <MenuList svg={billSVG}>Transaction</MenuList>
+                                            <MenuList svg={starSVG}>Review</MenuList>
+                                            <MenuList svg={heartBoxSVG}>Wishlist</MenuList>
+                                            <Button className="" text="Account" svg={profileSVG} />
+                                            <Button className="bg-slate-800 text-slate-50 hover:text-slate-800" text="Logout" svg={logoutSVG} />
+                                        </Dropdown>
                                     </div>
-                                </Modal>   
+                                :
+                                    // if user is logged out
+                                    <div>
+                                        <div className="gap-4 flex">
+                            
+                                            {/* Sign Up Modal */}
+                                            <Modal modalTitle="Sign Up As" textBtn="Sign Up"
+                                                modalID="sign-up-as-modal"
+                                                modalClassName="max-w-96 text-center"
+                                                buttonClassName="px-8 hidden md:flex"
+                                            >
+                                                <div className="flex flex-col gap-4">
 
-                                {/* Login Modal */}
-                                <Modal modalTitle="Login" textBtn="Login"
-                                    modalID="login-modal"
-                                    modalClassName="max-w-96"
-                                    buttonClassName="px-8 hidden md:flex"
-                                >
-                                    <LoginForm className="flex flex-col gap-4" />
-                                </Modal>
+                                                    {/* Sign Up as Organizer Modal */}
+                                                    <Modal modalTitle="Sign Up as Organizer" textBtn="Organizer"
+                                                        modalID="sign-up-modal-organizer"
+                                                        modalClassName="max-w-96"
+                                                        buttonClassName="w-full"
+                                                        modalIDToClose="sign-up-as-modal"
+                                                        formID="sign-up-form-organizer"
+                                                    >
+                                                        <SignUpForm className="flex flex-col sm:gap-4 gap-2" role="organizer"  />
+                                                    </Modal>
+                                                    <p className="text-center">or</p>
 
-                            </div>
-                        
-                            <Dropdown 
-                                className="md:hidden dropdown-end" 
-                                ulClassName=" flex gap-4"
-                                buttonSvg={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16"><path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/></svg>}>
-                                <li onClick={showSignUpModal}><a>Sign Up</a></li>
-                                <li onClick={showLoginModal}><a>Login</a></li>
-                            </Dropdown>
+                                                    {/* Sign Up as User Modal */}
+                                                    <Modal modalTitle="Sign Up as Adventurer" textBtn="Adventurer"
+                                                        modalID="sign-up-modal-adventurer"
+                                                        modalClassName="max-w-96"
+                                                        buttonClassName="w-full"
+                                                        modalIDToClose="sign-up-as-modal"
+                                                        formID="sign-up-form-user"
+                                                    >
+                                                        <SignUpForm className="flex flex-col sm:gap-4 gap-2" role="adventurer" />
+                                                    </Modal>
+                                                </div>
+                                            </Modal>   
+
+                                            {/* Login Modal */}
+                                            <Modal modalTitle="Login" textBtn="Login"
+                                                modalID="login-modal"
+                                                modalClassName="max-w-96"
+                                                buttonClassName="px-8 hidden md:flex"
+                                            >
+                                                <LoginForm className="flex flex-col gap-4" />
+                                            </Modal>
+
+                                        </div>
+                                    
+                                        <Dropdown 
+                                            className="md:hidden dropdown-end" 
+                                            ulClassName=" flex gap-4"
+                                            buttonSvg={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16"><path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/></svg>}>
+                                            <li onClick={showSignUpModal}><a>Sign Up</a></li>
+                                            <li onClick={showLoginModal}><a>Login</a></li>
+                                        </Dropdown>
+                                    </div>
+                                    
+                            }
+                            
                         </div>
                     :
-                        <div>
-                            <Button className="" text="Log Out" svg={logoutSvg} />
+                        // dashboard
+                        <div className="">
+                            
+                            {
+                                isUserLoggedNavbar ? // if user is logged in
+                                    
+                                    <div className=""> 
+                                        <div className="hidden lg:flex gap-4">
+                                            <div className="">
+                                                <Button className="bg-accent" text="Browse Festivites" svg={searchSVG} />
+                                            </div>
+                                            <div className="">
+                                                <Button className="" text="Log Out" svg={logoutSVG} />
+                                            </div>
+                                        </div>
+
+                                        <Dropdown 
+                                            className="dropdown-end lg:hidden" 
+                                            ulClassName=" flex gap-4"
+                                            buttonSvg={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16"><path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/></svg>}>
+                                            <Button className="" text="Browse Festivities" svg={searchSVG} />
+                                            <MenuList svg={calendarSVG}>Events</MenuList>
+                                            <MenuList svg={billSVG}>Transaction</MenuList>
+                                            <MenuList svg={starSVG}>Review</MenuList>
+                                            <MenuList svg={heartBoxSVG}>Wishlist</MenuList>
+                                            <Button className="" text="Account" svg={profileSVG} />
+                                            <Button className="bg-slate-800 text-slate-50 hover:text-slate-800" text="Logout" svg={logoutSVG} />
+                                        </Dropdown>
+
+                                    </div>
+                                :   
+                                    // if organizer is logged in
+                                    <div className="flex gap-4">
+                                        <Modal 
+                                            modalID={"event-modal"} 
+                                            modalTitle={"Create New Event"} 
+                                            textBtn="Create New Event"
+                                            buttonClassName="bg-accent hidden lg:flex"
+                                            modalClassName=" max-w-[60rem]"
+                                            btnSvg={plusSVG}
+                                        >
+                                            <EventForm className={""} />
+                                        </Modal>
+                                        
+                                        <Dropdown 
+                                            className="dropdown-end lg:hidden" 
+                                            ulClassName="flex gap-4"
+                                            buttonSvg={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16"><path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/></svg>}>
+                                            <Button className="" text="Create New Event" svg={plusSVG} />
+                                            <MenuList svg={calendarSVG}>Events</MenuList>
+                                            <MenuList svg={billSVG}>Promos</MenuList>
+                                            <MenuList svg={starSVG}>Analytics</MenuList>
+                                            <Button className="" text="Account" svg={profileSVG} />
+                                            <Button className="bg-slate-800 text-slate-50 hover:text-slate-800" text="Logout" svg={logoutSVG} />
+                                        </Dropdown>
+
+                                        <div>
+                                            <Button className="hidden lg:flex" text="Log Out" svg={logoutSVG} />
+                                        </div>
+                                    </div>
+                            }
+
                         </div>
                 }
-                
             </Navbar>
 
             { children }
