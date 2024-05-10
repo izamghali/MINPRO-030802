e565214bcca014194542a68af546131cb9511e9e
Izam Ghali <izamghali18@gmail.com>
2024-05-10 18:55:00 +0700

    feat: added store and auth slice on redux dir

---
+++ apps/web/src/redux/features/auth-slice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+type InitialState = {
+    value: {
+        isAuth: boolean
+    }
+}
+
+const initialState: InitialState = {
+    value: { // value of who is authenticated
+        isAuth: false, // 
+    }
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: { // collection of reducers of authSlice
+        testAut: () => {
+            return initialState;
+        }
+    }
+})
